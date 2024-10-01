411da4bc070ec7d2edb7e3084bfe0e6df43943aa
lotfi3003 <51326208+lotfi3003@users.noreply.github.com>
2024-10-01 23:44:23 +0200

    feat: modification on the table

--- src/employee/views/dashboard.vue
+++ src/employee/views/dashboard.vue
@@ -1,13 +1,15 @@
-<template> <v-card :title="`Dashboard for ${table_name}`" flat>
+<template>
+    <v-card :title="`${table_name} Dashboard`" flat>
         <template v-slot:text>
-            <v-text-field v-model="search" label="Search" prepend-inner-icon="mdi-magnify" variant="outlined"
+            <v-text-field v-model="search" label="Search" clearable prepend-inner-icon="mdi-magnify" variant="outlined"
                 hide-details single-line></v-text-field>
         </template>
-        <v-data-table :search="search" :headers="headers" :items="items" :sort-by="[{ key: 'calories', order: 'asc' }]">
+        <v-data-table :search="search" :headers="headers" :items="items" :sort-by="[{ key: 'calories', order: 'asc' }]"
+            loading-text="Loading... Please wait" :loading="loadign">
 
             <template v-slot:top>
                 <v-toolbar flat>
-                    <v-toolbar-title>My CRUD</v-toolbar-title>
+                    <v-toolbar-title>DATA</v-toolbar-title>
                     <v-divider class="mx-4" inset vertical></v-divider>
                     <v-spacer></v-spacer>
                     <v-dialog v-model="dialog" max-width="500px">
@@ -57,7 +59,7 @@
                     </v-dialog>
                 </v-toolbar>
             </template>
-            <template v-slot:[`item.actions`]="{ item }">
+            <template #item.actions="{ item }">
                 <v-icon class="me-2" size="small" @click="editItem(item)">
                     mdi-pencil
                 </v-icon>
@@ -84,6 +86,7 @@ const table_name = ref(employeeStore.selected_Table)
 const dialogDelete = ref(false)
 const headers = ref([])
 const search = ref()
+const loadign = ref(true)
 const items = ref([])
 
 const editedIndex = ref(-1)
@@ -141,7 +144,8 @@ onMounted(() => {
             //     { title: 'Delete', key: 'delete', sortable: false }
             // )
 
-            items.value = data
+            items.value = data; 
+            loadign.value = false
         })
 })
 
