1291f6c689b6d254838862728641d65534f3eb08
lotfi3003 <51326208+lotfi3003@users.noreply.github.com>
2024-10-02 14:46:04 +0200

    fix

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,9 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AppFooter: typeof import('./components/AppFooter.vue')['default']
-    Components: typeof import('./components/index.vue')['default']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
